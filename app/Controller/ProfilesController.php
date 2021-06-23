f33d95b2bd3d26b375c7700750ddee3ef898f57a
alloyking1 <alloyking1@gmail.com>
2021-06-23 19:06:36 -0400

    created the user profile, fetch user name and user post count in profile

---
+++ app/Controller/ProfilesController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProfilesController extends AppController {
+
+    var $uses = array('Post');
+
+    public function index(){
+
+        $data = array(
+            'profiles' => $this->Auth->user('username'),
+            // 'posts' => $this->Post->find('count'),
+            'posts' => $this->Post->find('count', array(
+                'conditions' => array('Post.user_id' => $this->Auth->user('id') )
+            ))
+        );
+        $this->set($data);
+    }
+}
---
+++ app/View/Profiles/index.ctp
@@ -0,0 +1,5 @@
+<h1>Welcome to your profile <?php echo $profiles; ?></h1>
+<p>You have a total number of <b><?php echo $posts ?></b> post</p>
+
+
+
